ab629f5adfcbb140403eaad67fbafb46472a4850
Carey Clark <careydclark@outlook.com>
2023-10-08 16:28:43 -0400

    CIS406 Initial Commit

---
+++ studentRegistration/src/studentRegistration/StudentRegistration.java
@@ -0,0 +1,12 @@
+package studentRegistration;
+
+public class StudentRegistration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println();
+		System.out.println("Congratulations on your first Java Program");
+	}
+
+}
